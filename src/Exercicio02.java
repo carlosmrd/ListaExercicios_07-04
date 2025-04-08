e99a0cc619209129743ca88cddf2b1f3b55c91c2
Carlos <carlosmrd2001@gmail.com>
2025-04-08 20:05:36 -0300

    Update Exercicio02

--- src/Exercicio02.java
+++ src/Exercicio02.java
@@ -2,7 +2,26 @@ import java.util.Scanner;
 
 public class Exercicio02 {
     public void executarExercicio02(Scanner scanner) {
+        //. Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor
+        //que C ou maior igual.
 
+        double a;
+        double b;
+        double c;
 
+        System.out.print("Insira o valor de A: ");
+        a = scanner.nextDouble();
+
+        System.out.print("Insira o valor de B: ");
+        b = scanner.nextDouble();
+
+        System.out.print("Insira o valor de C: ");
+        c = scanner.nextDouble();
+
+        if ((a + b) < c ) {
+            System.out.println("A soma de A+B é igual a " + (a + b) + " e é menor do que C.");
+        } else {
+            System.out.println("A soma de A+B é igual a " + (a + b) + " e é maior ou igual a C.");
+        }
     }
 }
