a9bda5cb4c0576d6fdcf075a10ff23fa009b3caf
Carlos <carlosmrd2001@gmail.com>
2025-04-09 23:13:15 -0300

    Update Exercícios

--- src/Exercicio03.java
+++ src/Exercicio03.java
@@ -3,6 +3,22 @@ import java.util.Scanner;
 public class Exercicio03 {
     public void executarExercicio03(Scanner scanner) {
 
+        //Dado um número, mostrar uma mensagem que ele é par e calcular o quadrado de dito número.
+        //Se ele é ímpar mostrar a mensagem que é ímpar e calcular o cubo de dito número.
 
+        double number;
+
+        System.out.print("Insira um número: ");
+        number = scanner.nextDouble();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " é um número par.");
+            number = Math.pow(number, 2);
+            System.out.println("Ao quadrado ele resulta " + number + ".");
+        } else {
+            System.out.println(number + " é um número ímpar.");
+            number = Math.pow(number, 3);
+            System.out.println("Ao cubo ele resulta " + number + ".");
+        }
     }
 }
--- src/Exercicio04.java
+++ src/Exercicio04.java
@@ -3,6 +3,67 @@ import java.util.Scanner;
 public class Exercicio04 {
     public void executarExercicio04(Scanner scanner) {
 
+        //Faça um algoritmo que leia o nome, o sexo e o estado civil de uma pessoa.
+        //• Caso sexo seja “(F)emenino” e estado civil seja “(C)asada”, solicitar o tempo de casada (anos).
+        //• As entradas para sexo será: (F)emenino ou (M)asculino; e para estado civil: (C)asado ou
+        //(S)olteiro
+        //• Ao final, exiba as informações.
 
+        String name;
+        char optionGender;
+        String gender;
+        char optionMaritalStatus;
+        String maritalStatus;
+        double timeMarried = 0;
+
+        System.out.print("Insira o nome da pessoa: ");
+        scanner.nextLine();
+        name = scanner.nextLine();
+
+        while (true) {
+            System.out.println("Selecione o sexo da pessoa: ");
+            System.out.println("M - Masculino");
+            System.out.println("F - Feminino");
+            optionGender = scanner.next().charAt(0);
+            if (optionGender == 'M') {
+                gender = "masculino";
+                break;
+            } else if (optionGender == 'F') {
+                gender = "feminino";
+                break;
+            } else {
+                System.out.println("Opção inválida. Insira novamente.");
+            }
+        }
+
+        while (true) {
+            System.out.println("Selecione o estado civil da pessoa");
+            System.out.println("S - Solteiro(a)");
+            System.out.println("C - Casado(a)");
+            optionMaritalStatus = scanner.next().charAt(0);
+            if (optionMaritalStatus == 'C') {
+                maritalStatus = "casado(a)";
+                break;
+            } else if (optionMaritalStatus == 'S'){
+                maritalStatus = "solteiro(a)";
+                break;
+            } else {
+                System.out.println("Opção inválida. Insira novamente.");
+            }
+        }
+
+        if (optionMaritalStatus == 'C') {
+            System.out.print("Quantos anos de casado? ");
+            timeMarried = scanner.nextDouble();
+        }
+
+        System.out.println();
+        System.out.printf("O nome da pessoa é %s.\n", name);
+        System.out.printf("%s é do sexo %s.\n", name, gender);
+        if (optionMaritalStatus == 'C'){
+            System.out.printf("%s é %s a %.0f ano(s).\n", name, maritalStatus, timeMarried);
+        } else {
+            System.out.printf("%s é %s.\n", name, maritalStatus);
+        }
     }
 }
